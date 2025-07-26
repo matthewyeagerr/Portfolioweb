c4daa0ea5812e6c9eda17b03fec5adbedc53426b
matthew yeager <matthewyeagerr@gmail.com>
2025-07-26 12:10:03 -0500

    removed clock and took off active tabs for individual project pages

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// Update clock every second
-function updateClock() {
-  const now = new Date();
-  let hours = String(now.getHours()).padStart(2, '0');
-  let minutes = String(now.getMinutes()).padStart(2, '0');
-  let seconds = String(now.getSeconds()).padStart(2, '0');
-  const timeString = `${hours}:${minutes}:${seconds}`;
-  document.getElementById("clock").textContent = timeString;
-}
-updateClock();
-setInterval(updateClock, 1000);
+
 
 
 // Tab switching with project detail reset
